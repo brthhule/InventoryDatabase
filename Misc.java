adc5555cdd5086960533cc7adc8e3cd92f302594
brthhule <brennenle@yahoo.com>
2023-08-27 18:26:41 -0400

    Working on more txt files

--- Misc.java
+++ Misc.java
@@ -9,21 +9,27 @@ public class Misc {
     }
 
     public static void showOptions(String filename) {
-        String filePath = "TxtFiles\\\\" + filename + ".txt";
+        String filePath = "src\\ShowOptions\\test.txt";
 
+        //String filePath = "src\\ShowOptions\\" + filename + ".txt";
+        File newFile = new File (filePath);
+        System.out.println("List of options:");
+        Scanner fileReader = null;
+        //Print options
         try {
-            File newFile = new File (filePath);
-            Scanner fileReader = new Scanner (newFile);
-            String line = fileReader.nextLine();
-            System.out.print(line);
+            fileReader = new Scanner (newFile);
+            String line = " ";
             while (fileReader.hasNextLine()) {
                 line = fileReader.nextLine();
-                System.out.print("\n" + line);
+                System.out.print(line + "\n");
             }
             fileReader.close();
+        } catch (FileNotFoundException d){
+            System.out.println("File does not exist...");
         } catch (Exception e) {
             System.out.println("Error occurred... please try again");
         }
+        System.out.print("Select an option: ");
     }
 
     public static String getInput(int highestNumber, String input) {
