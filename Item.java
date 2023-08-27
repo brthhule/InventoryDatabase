b95b6d455d94a7f9fb925c8c6dd64861f0c56a8d
patrickhong <path.plh@gmail.com>
2023-08-27 13:31:01 -0400

    Commit

--- Item.java
+++ Item.java
@@ -10,15 +10,26 @@ public class Item {
     }
 
     public String getName() {
+       return name;
     }
+
+    //std::string getName();
+    //int getAmount();
+    //void increaseAmount(int amountArg);
+
+    public void increaseAmount(int amountArg){
+        amount += amountArg;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+
+
 }
-    std::string getName();
-    int getAmount();
-    void increaseAmount(int amountArg);
 
-
-};
-
+/*
 Item() {
         name = "NULL";
         amount = NULL;
@@ -40,3 +51,4 @@ Item() {
         void Item::increaseAmount(int amountArg) {
         amount += amountArg;
         }
+*/
